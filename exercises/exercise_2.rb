057263ff24083ee53f359dc02e52966c85ffac93
PatrickRanval <pat.ranval@gmail.com>
2024-01-10 17:56:12 +0000

    "Exercise 2 Work"

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -2,19 +2,22 @@
 # However, the current implementation is faulty and does not produce the correct result.
 
 def calculate_average(numbers)
-
+  count = numbers.count()
+  sum = 0
+  numbers.each do |num|
+    sum += num
+  end
+  sum / count
 end
 
-# 2b. The script is supposed to define a method filter_long_strings that takes an array of strings and an integer (length). 
+# 2b. The script is supposed to define a method filter_long_strings that takes an array of strings and an integer (length).
 # The method should return a new array containing only the strings that are longer than the specified length. However, the current implementation is faulty and does not produce the correct result.
 
-def filter_long_strings(strings, length)
+def filter_long_strings(strings, length); 
 
 end
 
-# 2c. The script is intended to define a method find_max that takes an array of numbers and returns the largest number in the array. 
+# 2c. The script is intended to define a method find_max that takes an array of numbers and returns the largest number in the array.
 # The current implementation, however, has one or more bugs.
 
-def find_max(numbers)
-
-end
+# def find_max(numbers); end
